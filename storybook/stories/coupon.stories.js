7edabb715ae856b77fe9bc09098a39c401360634
Miguel Viñan H <mvinan@icloud.com>
2018-03-25 11:49:50 -0500

    add coupon component

--- storybook/stories/coupon.stories.js
+++ storybook/stories/coupon.stories.js
@@ -11,5 +11,12 @@ const story = storiesOf('Coupon', module)
 story.addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
 
 story.add('Simple Coupon', () => (
-  <Coupon />
+  <Coupon
+    imageSource={{ uri: "https://images.unsplash.com/photo-1481070414801-51fd732d7184?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=82203e4d57fc0d3bdd8ffc0f66d09763&auto=format&fit=crop&w=1525&q=80" }}
+    avatarSource={{ uri: "https://images.unsplash.com/profile-1481466571593-63d100a3cbd1?dpr=2&auto=format&fit=crop&w=64&h=64&q=60&cs=tinysrgb&crop=faces&bg=fff" }}
+    numberOfCoupons={ 50 }
+    title="2x1 en Hamburguesas Mexicanas"
+    subTitle="Carbon Burguer"
+    date="11 de Marzo - 12 de Abril"
+  />
 ))
